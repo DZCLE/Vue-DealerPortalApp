8dc7e9cce8b199fc99aff0cd177bf00bbefbb795
NGIC\I824286 <dean.zhang@ngic.com>
2022-09-22 17:55:09 -0400

    more eslint

--- BoilerPlate.UI/.eslintrc.js
+++ BoilerPlate.UI/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+npmmodule.exports = {
     "env": {
         "browser": true,
         "es6": true
@@ -12,6 +12,7 @@ module.exports = {
         "SharedArrayBuffer": "readonly"
     },
     "parser": "vue-eslint-parser",
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
